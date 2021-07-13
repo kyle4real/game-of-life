893611578366f50b01a17e7daefe79731b2cf3ea
Kyle Friel <businessfriel@gmail.com>
2021-07-12 22:40:26 -0400

    Simulation logic.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import { produce } from "immer";
 const numRows = 50;
 const numCols = 50;
 
+const operations = [
+    [0, 1],
+    [0, -1],
+    [1, -1],
+    [-1, 1],
+    [1, 1],
+    [-1, -1],
+    [1, 0],
+    [-1, 0],
+];
+
 function App() {
     const [grid, setGrid] = useState(() => {
         const rows = [];
@@ -25,6 +36,33 @@ function App() {
         if (!runningRef.current) {
             return;
         }
+
+        // simulation logic
+        setGrid((g) => {
+            return produce(g, (gridCopy) => {
+                for (let i = 0; i < numRows; i++) {
+                    for (let j = 0; j < numCols; j++) {
+                        let neighbors = 0;
+                        // this will tell us how many neighbors a given cell has
+                        // the alternative would be to do a bunch of if statements to check each neighbor
+                        operations.forEach(([x, y]) => {
+                            const newI = i + x;
+                            const newJ = j + y;
+                            if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
+                                neighbors += g[newI][newJ];
+                            }
+                        });
+
+                        if (neighbors < 2 || neighbors > 3) {
+                            gridCopy[i][j] = 0;
+                        } else if (g[i][j] === 0 && neighbors === 3) {
+                            gridCopy[i][j] = 1;
+                        }
+                    }
+                }
+            });
+        });
+
         // simulate
         setTimeout(runSimulation, 1000);
     }, []);
